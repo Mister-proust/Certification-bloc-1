734c9560e4d86bfaad353ad3a350d323b1c0c4be
Mister-proust <maximilien_proust@hotmail.fr>
2025-06-04 16:56:22 +0200

    Ajout d'un fichier CSV sur la démographie par département

---
+++ Extraction/CSV/insertion_eff_total.py
@@ -0,0 +1,65 @@
+import pandas as pd
+from sqlalchemy import create_engine, text
+from dotenv import load_dotenv
+import os
+
+load_dotenv(dotenv_path="../../.env", override=True)
+
+USER = os.getenv("USER_POSTGRES")
+PASSWORD = os.getenv("PASSWORD_POSTGRES")
+HOST = os.getenv("HOST_POSTGRES")
+PORT = os.getenv("PORT_POSTGRES")
+DATABASE = os.getenv("DATABASE_POSTGRES")
+
+user = USER
+password = PASSWORD
+host = HOST
+port = PORT
+database = DATABASE
+schema = "Pollution_Cancer"
+table = "Effectif_departement"
+table2 = "Metada_effectif_departement"
+
+
+df = pd.read_csv("../../data/eff_pop.csv", sep=";")
+
+df_metadata = pd.read_csv("../../data/metadata_eff_pop.csv", sep=";")
+
+df = df[df["GEO_OBJECT"] == "DEP"]
+
+df= df.rename(columns={
+    "GEO": "Num_dep",
+    "SEX" : "Sexe",
+    "GEO_OBJECT": "Carac_dep",
+    "AGE": "Age",
+    "EP_MEASURE": "Carac_mesure",
+    "OBS_STATUS_FR": "Chiffre_def",
+    "TIME_PERIOD": "Annee",
+    "OBS_VALUE": "Effectif" })
+
+engine = create_engine(f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}")
+
+with engine.connect() as conn:
+    conn.execute(text(f'CREATE SCHEMA IF NOT EXISTS "{schema}";'))
+    conn.execute(text(f'SET search_path TO {schema};'))
+    
+    df.to_sql(
+        table,
+        con=conn,
+        schema=schema,
+        index=False,
+        if_exists='replace',  
+        method='multi'
+    )
+
+    df_metadata.to_sql(
+        table2,
+        con=conn,
+        schema=schema,
+        index=False,
+        if_exists='replace',  
+        method='multi'
+    )
+    conn.commit()
+
+print(f"Données insérées avec succès, veuillez vérifier.")
--- README.md
+++ README.md
@@ -9,3 +9,8 @@ Scrapping : https://www.sagepesticides.qc.ca/Recherche/RechercheMatiere
 
 API : https://hubeau.eaufrance.fr/page/api-vente-et-achat-de-produits-phytopharmaceutiques#/Achat/getAchatSubstance
 
+CSV : données démographiques : https://catalogue-donnees.insee.fr/fr/catalogue/recherche/DS_ESTIMATION_POPULATION
+
+Données sur les pathologies : https://data.ameli.fr/explore/dataset/effectifs/table/
+
+Données sur l'AMM des substances et produits chimiques : https://www.data.gouv.fr/fr/datasets/donnees-ouvertes-du-catalogue-e-phy-des-produits-phytopharmaceutiques-matieres-fertilisantes-et-supports-de-culture-adjuvants-produits-mixtes-et-melanges/#/resources
